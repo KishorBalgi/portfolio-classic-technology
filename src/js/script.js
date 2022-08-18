5363d1898cdea3aa8c2a5451ecde15ca7139690f
Kishor Balgi <kishorbalgi@gmail.com>
2022-08-18 19:57:48 +0530

    Updated UI of services and recent posts

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// Glass Card tile animation:
+VanillaTilt.init(document.querySelectorAll(".tilt-card"), {
+  max: 15,
+  speed: 1000,
+  transition: true,
+  glare: true,
+  reverse: true,
+});
+
+VanillaTilt.init(document.querySelectorAll(".blog-card"), {
+  max: 10,
+  speed: 1000,
+  transition: true,
+  glare: true,
+  reverse: true,
+});
